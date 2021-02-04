589e6c627f2b75e271d26ff58f4cd34546997044
Aleksander Filek <filekaleksander@gmail.com>
2021-02-04 21:46:45 +0100

    time class added

---
+++ include/H_time.hpp
@@ -0,0 +1,27 @@
+#ifndef H_timer_HPP
+#define H_timer_HPP
+
+namespace Hero
+{
+
+class Time
+{
+    private:
+        static Time* _instance;
+
+        double _deltaTime;
+        double _timeScale;
+
+        static void SetDeltaTime(double value){ _instance->_deltaTime = value;}
+
+    public:
+        Time();
+
+        static double GetDeltaTime(){ return _instance->_deltaTime*_instance->_timeScale; }
+        static void SetScale(double value){ _instance->_timeScale = value; }
+        static double GetScale(){ return _instance->_timeScale; }
+};
+
+}
+
+#endif
---
+++ source/H_time.cpp
@@ -0,0 +1,8 @@
+#include"H_time.hpp"
+namespace Hero
+{
+Time::Time()
+{
+    _instance = this;
+}
+}
