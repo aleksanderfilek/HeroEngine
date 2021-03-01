7f2ce3409aac288a71deedfa4d62ac8da373bf9f
unknown <filekaleksander@gmail.com>
2021-03-01 19:49:24 +0100

    basic opengl error handling

--- include/H_stdlib.hpp
+++ include/H_stdlib.hpp
@@ -4,6 +4,9 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
+#include<GL\glew.h>
+#include<iostream>
+#include<string>
 
 #ifdef DEBUG
     #define IF_DEBUG_CODE(...) __VA_ARGS__
@@ -11,6 +14,10 @@
     #define IF_DEBUG_CODE(...)
 #endif
 #define DEBUG_CODE(code) IF_DEBUG_CODE( code )   
+
+
+GLenum glCheckError_(const char *file, int line);
+#define glCheckError() glCheckError_(__FILE__, __LINE__) 
 
 namespace Hero
 {
